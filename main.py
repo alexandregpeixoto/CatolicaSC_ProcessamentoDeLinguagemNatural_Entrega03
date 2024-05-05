063e26447fa763b3b9c29917e783d4ba615a0338
alexandregpeixoto <alexandregpeixoto@gmail.com>
2024-05-05 14:26:19 -0300

    Entrega 03

--- main.py
+++ main.py
@@ -32,15 +32,22 @@ def analisar_sentimento(frase):
     else:
         return 'neutro'
 
-# Solicitar termo ao usuário
-termo = input("Digite o termo que deseja buscar na transcrição: ")
-
-# Buscar termo na transcrição e mostrar resultados
-resultados = buscar_termos(termo)
-if len(resultados) > 0:
-    print(f"Resultados para o termo '{termo}':")
-    for pessoa, texto in resultados:
-        sentimento = analisar_sentimento(texto)
-        print(f'{pessoa}: {texto} - Sentimento: {sentimento}')
-else:
-    print(f"O termo '{termo}' não foi encontrado na transcrição.")
+# Loop principal
+while True:
+    # Solicitar termo ao usuário
+    termo = input("Digite o termo que deseja buscar na transcrição ('sair' para encerrar): ")
+    
+    # Verificar se o usuário deseja sair
+    if termo.lower() == 'sair':
+        print("Encerrando o programa...")
+        break
+    
+    # Buscar termo na transcrição e mostrar resultados
+    resultados = buscar_termos(termo)
+    if len(resultados) > 0:
+        print(f"Resultados para o termo '{termo}':")
+        for pessoa, texto in resultados:
+            sentimento = analisar_sentimento(texto)
+            print(f'{pessoa}: {texto} - Sentimento: {sentimento}')
+    else:
+        print(f"O termo '{termo}' não foi encontrado na transcrição.")
